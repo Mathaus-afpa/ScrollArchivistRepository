78b8f47f0ec71c27e9316c17d9c68c3fa4c94a63
Mathaus-afpa <mathieu.hoarau@afpa-dev-pompey.fr>
2024-08-08 08:35:28 +0200

    Initial commit New Java Project ScrollArchivistRepository (SAR) feat(SAR, ScrollArchivistRepository.java, CREATE, class): Ajout de la classe d'entree. feat(SAR, ScrollArchivistRepository.java, CREATE, function): Ajout de la fonction main.

---
+++ src/scrollarchivistrepository/ScrollArchivistRepository.java
@@ -0,0 +1,14 @@
+package scrollarchivistrepository;
+/**
+ * [ScroolArchivistRepository] - entry class
+ * @author Mathaus
+ */
+public class ScrollArchivistRepository {
+    /**
+     * Entry point
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
